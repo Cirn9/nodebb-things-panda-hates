19d9a80176bc08e8dce38e9182e721a2c7734b02
Cirn9 <cirno@cirn9.info>
2015-05-31 16:32:13 -0400

    shell

---
+++ library.js
@@ -0,0 +1,9 @@
+(function(module){
+	var thingsPandaHates = {};
+	
+	thingsPandaHates.parse = function(template, url) {
+		console.log("template: " + template + "\nurl: " + url + "\n");
+	}
+	
+	module.exports = thingsPandaHates;
+}(module));
